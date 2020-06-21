01937eb145a6ef7a2dfe22f5db1c057407c9d5d0
Takahiro Ushijima <v19e3004@oita-u.ac.jp>
2020-06-21 10:59:31 +0000

    Lesson07の課題

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,8 @@ class PostsController < ApplicationController
     def index
         @posts = Post.all.order(created_at: 'desc') #新しいもの順に並べる
     end
+    
+    def show
+        @post = Post.find(params[:id])
+    end
 end
