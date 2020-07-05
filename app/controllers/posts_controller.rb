2425b74775167599e1ed5b01ff41993e629bce9e
Takahiro Ushijima <v19e3004@oita-u.ac.jp>
2020-07-05 17:13:49 +0000

    Lesson09の課題

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,32 @@ class PostsController < ApplicationController
     def show
         @post = Post.find(params[:id])
     end
+    
+    def new
+        @post = Post.new
+    end
+    
+    def create
+        #ender plain: params[:post].inspect
+        #save
+        #@post = Post.new(params[:post])
+        #@post = Post.new(params.require(:post).permit(:title, :body))
+        @post = Post.new(post_params)
+        if @post.save
+            #redirect
+            redirect_to posts_path
+        else
+            #render plain: @post.errors.inspect
+            render 'new'
+        end
+    end
+    
+    def edit
+        @post = Post.find(params[:id])
+    end
+    
+    private
+        def post_params
+            params.require(:post).permit(:title, :body)
+        end
 end
